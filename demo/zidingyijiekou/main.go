7d47e8f137101a20ca88f60c949dcae60e60d6bf
diudiu <darlingxiaohang@gmail.com>
2023-01-10 18:21:55 +0800

    2023-01-10

--- demo/zidingyijiekou/main.go
+++ demo/zidingyijiekou/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	//创建一个新接口,名字不能是一个变量
+	type Weekday int
+	const ( //常量，一定要首字母大写
+		Monday Weekday = iota + 1
+		Tuesday
+		Wednesday
+		Thursday
+		Friday
+		Saturday
+		Sunday
+	)
+
+	fmt.Println(Sunday)
 
 }
